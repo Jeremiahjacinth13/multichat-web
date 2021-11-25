9b746a9ebc32828d76e3a982b8792a7b8570be37
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-11-25 07:36:03 +0100

    initialized firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKLTCjJnE3YkP0l_YxaFdUbj4MNnhYTb4",
+    authDomain: "multichat-551c1.firebaseapp.com",
+    databaseURL: "https://multichat-551c1-default-rtdb.firebaseio.com",
+    projectId: "multichat-551c1",
+    storageBucket: "multichat-551c1.appspot.com",
+    messagingSenderId: "838695630643",
+    appId: "1:838695630643:web:e019fea0e8fd460ca9f046",
+    measurementId: "G-D921840W9V"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
