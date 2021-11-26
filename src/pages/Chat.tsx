03e0ae0d9f68508bb08aeea99a239e474dc6e923
Lena Jeremiah <49532117+Jeremiahjacinth13@users.noreply.github.com>
2021-11-26 04:41:59 +0100

    commit

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,17 +1,71 @@
-import React from 'react'
-import { UserContext, UserContextType } from '../UserContext'
+import React, { useEffect } from 'react'
+import {getAuth} from 'firebase/auth'
+import { getDatabase, ref, onValue } from 'firebase/database'
+import './styles.css'
+import { Header } from './Header'
+
+
+type Message = {
+    text: string
+    createdAt: number,
+    senderImage?: string,
+    senderLetter?: string,
+    senderId: string
+}
+
+const getMessagesFromRequest = (requestMessages: any[]): Message[] => {
+    return requestMessages.map(requestMessage => ({
+        text: requestMessage.text,
+        createdAt: requestMessage.createdAt,
+        senderImage: requestMessage.sender.image,
+        senderLetter: requestMessage.sender.letter,
+        senderId: requestMessage.sender.id
+    }))
+}
 
 const Chat: React.FC = function () {
 
-    const {logoutUser} = React.useContext<UserContextType>(UserContext)
+    const [messages, setMessages] = React.useState<Message[]>([])
+    const db = getDatabase()
+
+    useEffect(() => {
+        (async function () {
+            const messagesRef = ref(db, 'messages')
+            onValue(messagesRef, snapshot => {
+                let messagesData = Object.values(snapshot.val())
+                setMessages(getMessagesFromRequest(messagesData))
+            })
+        })()
+    }, [])
 
     return (
-        <div className='chatScreen'>
-            <h1>This is the chat page</h1>
-            {
+        <div className='chat'>
+            <Header />
+            <ChatArea messages = {messages}/>
+        </div>
+    )
+}
 
+const ChatArea: React.FC<{messages: Message[]}> = ({messages}) => {
+
+    return (
+        <div className='chatArea container mx-auto'>
+            {
+                messages.map(message => <ChatMessage message={message} />)
             }
-            <button onClick={() => logoutUser()}></button>
+        </div>
+    )
+}
+const ChatMessage: React.FC<{ message: Message }> = ({ message }) => {
+
+    const auth = getAuth()
+
+    return (
+        <div className = {`chat ${message.senderId === auth.currentUser?.uid ? 'self' : 'notSelf'}`}>
+            <p>Text: {message.text}</p>
+            <p>SenderImage: <img src={message.senderImage} /></p>
+            <p>SenderLetter: {message.senderLetter}</p>
+            <p>Created At: {new Date(message.createdAt).toDateString()}</p>
         </div>
     )
 }
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext, UserContextType } from '../UserContext'
 import './styles.css'
 
 export const Header: React.FC = () => {
+
+    const {logoutUser} = React.useContext<UserContextType>(UserContext)
+    
     return (
         <header className='header'>
             <div className="container mx-auto">
                 <Link to='/' className='multichatLogo'>MC.</Link>
+                <button onClick={() => logoutUser()}>Logout</button>
             </div>
         </header>
     )
